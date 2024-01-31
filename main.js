46bc9cc467834442fd9fa9a6f7ab7cf2bb67ad28
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-01-31 17:01:11 +0100

    Add dummy image into list item to preview and adjust css

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-'use strict'
+"use strict";
 
-const brightness = document.querySelector('.brightness')
-const main = document.querySelector('main')
-const countryList = document.querySelector('.country__list')
+const brightness = document.querySelector(".brightness");
+const main = document.querySelector("main");
+const countryList = document.querySelector(".country__list");
 
 const showCountries = () => {
-    const countries= document.createElement('li')
-    
-}
+  const country = document.createElement("li");
+//   countryList.innerHTML = "";
+  country.classList.add("country__list__item");
+  country.innerHTML = `<img src="public/img/Flag_of_Montenegro.svg.png">
+    <h2>Montenegro</h2>
+    `;
+    countryList.appendChild(country)
+};
+
+showCountries()
+showCountries()
+showCountries()
+showCountries()
