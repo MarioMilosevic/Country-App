5140abe48752930b1e0fb4dcee17a4fd1166d6ce
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-01-31 20:07:46 +0100

    Fetch countries and render all of them

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 "use strict";
-
+//  https://restcountries.com/v3.1/all
 const brightness = document.querySelector(".brightness");
 const main = document.querySelector("main");
 const countryList = document.querySelector(".country__list");
 
-const showCountries = () => {
-  const country = document.createElement("li");
-//   countryList.innerHTML = "";
-  country.classList.add("country__list__item");
-  country.innerHTML = `<img src="public/img/Flag_of_Montenegro.svg.png">
-    <h2>Montenegro</h2>
+const showCountries = (arr) => {
+  arr.forEach(obj => {
+    const country = document.createElement("li");
+    //   countryList.innerHTML = "";
+    country.classList.add("country__list__item");
+    console.log(obj);
+    country.innerHTML = `<img src="${obj.flags.svg}">
+    <h2>${obj.name.common}</h2>
     `;
     countryList.appendChild(country)
+  })
 };
 
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
-showCountries()
+async function findCountry(){
+ const response = await fetch( 'https://restcountries.com/v3.1/all')
+ const countries = await response.json()
+ console.log(countries);
+ showCountries(countries)
+}
+
+findCountry()
