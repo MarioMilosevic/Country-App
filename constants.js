331f6c7f75d722503ac93a14b425c928d8fe86ea
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-02-14 22:13:48 +0100

    prod

---
+++ constants.js
@@ -0,0 +1,2 @@
+'use strict'
+export const url = `https://restcountries.com/v3.1`;
--- main.js
+++ main.js
@@ -19,13 +19,17 @@ import {
 } from "/helperFunctions/helperFunctions";
 import { Countries } from "/Classes/Countries";
 import { DarkMode } from "/Classes/DarkMode";
-import { Router } from "/Router/Router";
-export const router = new Router();
+import { Router } from "./router";
+import { url } from "./constants";
 
-
-
-export const main = document.querySelector("main");
 const brightness = document.querySelector("#brightness");
+const countryRegion = document.querySelector(".country__region");
+const countryImg = document.querySelector("#country__app__logo");
+const countrySearch = document.querySelector("#country__search");
+const previousPage = document.querySelector(".previousPage");
+const nextPage = document.querySelector(".nextPage");
+const pageButtons = document.querySelectorAll(".listBtn");
+export const main = document.querySelector("main");
 export const searchBarContainer = document.querySelector(
   ".search__bar__container"
 );
@@ -33,25 +37,21 @@ export const countryApp = document.querySelector(".country__app");
 export const renderedCountry = document.querySelector(".rendered__country");
 export const brightnessImg = brightness.querySelector("img");
 export const brightnessText = brightness.querySelector("p");
-const countrySearch = document.querySelector("#country__search");
 export const countryList = document.querySelector(".country__list");
-const countryRegion = document.querySelector(".country__region");
-const countryImg = document.querySelector("#country__app__logo");
 export const pageList = document.querySelector(".pageList");
 export const pageNumbers = document.querySelector(".pageNumbers");
+export const firstPageBtn = document.querySelector(".listBtn");
+
 export const darkMode = new DarkMode();
+export const router = new Router();
 export const countries = new Countries();
 let page = 0;
-const previousPage = document.querySelector(".previousPage");
-const nextPage = document.querySelector(".nextPage");
 
 window.addEventListener("DOMContentLoaded", function () {
   router.init();
 });
 
-const fetchedCountries = await findCountry(
-  "https://restcountries.com/v3.1/all"
-);
+const fetchedCountries = await findCountry(`${url}/all`);
 
 countries.setCountries(fetchedCountries);
 countries.sortCountries();
@@ -60,18 +60,16 @@ showCountries(countries.get24Countries("1").sort());
 renderPageButtons(countries.getCountries(), pageNumbers);
 countryList.addEventListener("click", (e) => showCountry(e));
 
-export const firstPageBtn = document.querySelector(".listBtn");
-const pageButtons = document.querySelectorAll(".listBtn");
 firstPageBtn.classList.add("clickedDark", "clicked");
 
 countryRegion.addEventListener("change", async () => {
   const region = countryRegion.value;
   countryList.innerHTML = "";
-  const urlAll = "https://restcountries.com/v3.1/all";
-  const url = `https://restcountries.com/v3.1/region/${region}`;
+  const urlAll = `${url}/all`;
+  const url = `${url}/region/${region}`;
   if (region === "All") {
     const responseAll = await findCountry(urlAll);
-    router.go("firstPage")
+    router.go("firstPage");
     countries.setCountries(responseAll);
     countries.sortCountries();
     showCountries(countries.get24Countries("1"));
@@ -81,7 +79,7 @@ countryRegion.addEventListener("change", async () => {
     firstPageBtn.classList.add("selected");
   } else {
     const response = await findCountry(url);
-    router.go(region)
+    router.go(region);
     countries.setCountries(response);
     countries.sortCountries();
     showCountries(countries.get24Countries("1").sort());
@@ -98,8 +96,9 @@ countrySearch.addEventListener("input", async function () {
   updateDebounceText(async () => {
     const searchName = countrySearch.value.toLowerCase();
     if (searchName.length > 0) {
-      const urlCountryName = `https://restcountries.com/v3.1/name/${searchName}`;
+      const urlCountryName = `${url}/name/${searchName}`;
       let searchedCountries = await findCountry(urlCountryName);
+      // ///////////////////
       countries.setCountries(searchedCountries);
       countries.sortCountries();
       countryList.innerHTML = "";
@@ -111,7 +110,7 @@ countrySearch.addEventListener("input", async function () {
       firstPageBtn.classList.add("clickedDark", "clicked");
     }
     if (searchName.length < 1) {
-      const urlAll = "https://restcountries.com/v3.1/all";
+      const urlAll = `${url}/all`;
       let searchedCountries = await findCountry(urlAll);
       countryList.innerHTML = "";
       countries.setCountries(searchedCountries);
